f415a55f0d6c043fa9a4f20a72daa2f1ff59cb71
Nuzael <juulioemanuel@gmail.com>
2023-08-12 18:26:28 -0300

    feat: add weather forecast

--- core/views.py
+++ core/views.py
@@ -6,7 +6,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-appid = os.getenv('API_KEY')
+appid = os.getenv('API_KEY') 
 
 def index(request):
     if request.method == 'GET':
@@ -21,6 +21,7 @@ def index(request):
         r_forecast_weather = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={appid}&lang=pt_br&units=metric')
         
         
+        # Checking if it's raining
         if r_current_weather.json().get('rain'):
             if r_current_weather.json().get('rain').get('3h'):
                 rain_value = r_current_weather.json()['rain']['3h']
@@ -28,7 +29,8 @@ def index(request):
                 rain_value = r_current_weather.json()['rain']['1h']
         else:
             rain_value = 0
-            
+        
+        # Current weather information
         current_weather = {
             'current_temp': round(r_current_weather.json()['main']['temp']),
             'icon': f"https://openweathermap.org/img/wn/{r_current_weather.json()['weather'][0]['icon']}@2x.png",
@@ -40,9 +42,77 @@ def index(request):
             'rain': rain_value,
         }
         
-        print(r_forecast_weather.json())
+        forecast_weather = [
+            {
+                'day': 'second',
+                'temp': [],
+                'max_temp': 0,
+                'min_temp': 0,   
+            },
+            {
+                'day': 'third',
+                'temp': [],
+                'max_temp': 0,
+                'min_temp': 0,   
+            },
+            {
+                'day': 'fourth',
+                'temp': [],
+                'max_temp': 0,
+                'min_temp': 0,   
+            },
+            {
+                'day': 'fifth',
+                'temp': [],
+                'max_temp': 0,
+                'min_temp': 0,   
+            },
+            {
+                'day': 'sixth',
+                'temp': [],
+                'max_temp': 0,
+                'min_temp': 0,   
+            },
+        ]
         
+        for item in r_forecast_weather.json()['list']:
+            # Return Y/M/D  
+            delta_time = item['dt_txt'][:10]
+            
+            # Separates data for each day
+            if str(date.today() + timedelta(days=0)) == delta_time:
+                pass
+            
+            elif str(date.today() + timedelta(days=1)) == delta_time:
+                forecast_weather[0]['temp'].append(item['main']['temp'])
+                forecast_weather[0]['max_temp'] = round(max(forecast_weather[0]['temp']))
+                forecast_weather[0]['min_temp'] = round(min(forecast_weather[0]['temp']))
+                
+            elif str(date.today() + timedelta(days=2)) == delta_time:
+                forecast_weather[1]['temp'].append(item['main']['temp'])
+                forecast_weather[1]['max_temp'] = round(max(forecast_weather[1]['temp']))
+                forecast_weather[1]['min_temp'] = round(min(forecast_weather[1]['temp']))
+                
+            elif str(date.today() + timedelta(days=3)) == delta_time:
+                forecast_weather[2]['temp'].append(item['main']['temp'])
+                forecast_weather[2]['max_temp'] = round(max(forecast_weather[2]['temp']))
+                forecast_weather[2]['min_temp'] = round(min(forecast_weather[2]['temp']))
+                
+            elif str(date.today() + timedelta(days=4)) == delta_time:
+                forecast_weather[3]['temp'].append(item['main']['temp'])
+                forecast_weather[3]['max_temp'] = round(max(forecast_weather[3]['temp']))
+                forecast_weather[3]['min_temp'] = round(min(forecast_weather[3]['temp']))
+                
+            elif str(date.today() + timedelta(days=5)) == delta_time:
+                forecast_weather[4]['temp'].append(item['main']['temp'])
+                forecast_weather[4]['max_temp'] = round(max(forecast_weather[4]['temp']))
+                forecast_weather[4]['min_temp'] = round(min(forecast_weather[4]['temp']))
+                
+            else:
+                break
+            
     context = {
-        'current_weather': current_weather
+        'current_weather': current_weather,
+        'forecast_weather': forecast_weather,
     }   
     return render(request, 'core/index.html', context=context)
--- templates/static/js/scripts.js
+++ templates/static/js/scripts.js
@@ -16,5 +16,27 @@ function updateDatetime() {
     spanDatetime.textContent = formattedTime;
 }
 
+function setDayOfTheWeek() {
+    const currentDate = new Date();
+    const currentDayIndex = currentDate.getDay();
+
+
+    const daysOfWeek = [
+        "Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"
+    ];
+
+    const orderedDaysOfWeek = [
+        ...daysOfWeek.slice(currentDayIndex),
+        ...daysOfWeek.slice(0, currentDayIndex)
+    ];
+
+    const forecastTitleElements = document.querySelectorAll('#forecast-title');
+
+    forecastTitleElements.forEach((element, index) => {
+        element.textContent = orderedDaysOfWeek[index+1];
+    });
+}
+
 setInterval(updateDatetime, 1000);
+setDayOfTheWeek();
 updateDatetime();
